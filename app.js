953483d60a4d71cf6777e7e1b19e7c3e9045f6cb
PeterianGod <740967459@qq.com>
2021-07-12 16:45:48 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// app.js
+const AV = require('./libs/av-core-min');
+const adapters = require('./libs/leancloud-adapters-weapp.js');
+
+AV.setAdapters(adapters);
+AV.init({
+  appId: 'MlCSwp0oETWm3o6LQeqF3k2U-gzGzoHsz',
+  appKey: 'f8WfT3yLhAjX82OB28Ipgqr8',
+  // 请将 xxx.example.com 替换为你的应用绑定的自定义 API 域名
+  serverURLs: "https://mlcswp0o.lc-cn-n1-shared.com",
+});
+
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
